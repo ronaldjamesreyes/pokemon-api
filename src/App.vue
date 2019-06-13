4ae5a683c84cf8bcb2dd8ab3bae069e21dae6032
Zankurou <48842512+ronaldreyes224@users.noreply.github.com>
2019-06-13 13:53:00 -0700

    adding rock types

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Card></Card>
+    <div class="container">
+      <h1 class="text-center display-1 m-5">Rock Type Pokemon</h1>
+      <div class="row">
+        <Card :url="item.pokemon.url" v-for="item in pokemon_rocktype" :key="item.pokemon.name"></Card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     Card
+  },
+  data: function () {
+    return {
+      pokemon_rocktype:""
+    }
+  },
+  mounted: function() {
+    console.log("mounted function ran")
+    const axios = require('axios');
+    const vm = this;
+    axios({
+        method: 'get',
+        url: 'https://pokeapi.co/api/v2/type/rock',
+        responseType: 'stream'
+    })
+    .then(function (response) {
+        vm.pokemon_rocktype = response.data.pokemon
+    });
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,46 +1,12 @@
 <template>
-    <div>
+    <div class="col-3">
         <div class="card" style="width: 18rem;">
-            <img v-bind:src="pokemon.sprites.front_default" class="card-img-top" alt="...">
+            <img v-bind:src="pokemon.sprites.front_shiny" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">{{pokemon.name}}</h5>
+                <h5 class="card-title text-capitalize">{{pokemon.name}}</h5>
                 <p class="pokemon-height">Height: {{pokemon.height*3.937}} units <br> Weight: {{pokemon.weight}} units</p>
             </div>
         </div>
-
-<div class="container">
-    <div class="row">
-      <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{pokemon.name}}</h5>
-                <p class="pokemon-height">Height: {{pokemon.height}} <br> Weight: {{pokemon.weight}}</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/26.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{pokemon.name}}</h5>
-                <p class="pokemon-height">Height: {{pokemon.height}} <br> Weight: {{pokemon.weight}}</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/172.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{pokemon.name}}</h5>
-                <p class="pokemon-height">Height: {{pokemon.height}} <br> Weight: {{pokemon.weight}}</p>
-            </div>
-        </div>
-    </div>
-  </div>
-</div>
-
-
     </div>
 </template>
 
@@ -50,11 +16,11 @@ export default {
   name: 'Card',
   data: function(){
       return{
-          pokemon:null
+          pokemon:""
       }
   },
   props: {
-    msg: String
+    url: String
   },
   mounted: function(){
     console.log("mounted function ran")
@@ -64,11 +30,10 @@ export default {
 
     axios({
         method: 'get',
-        url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
-         responseType: 'stream'
+        url: vm.url,
+        responseType: 'stream'
     })
     .then(function (response){
-        console.log(response.data);
         vm.pokemon = response.data
     });
 
@@ -78,18 +43,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card{
+    text-align: center;
+    margin-bottom: 10px;
 }
 </style>
