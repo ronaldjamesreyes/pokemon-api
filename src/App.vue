908d7a987206ceb02c59a0c237af88e3ad9e2665
Zankurou <48842512+ronaldreyes224@users.noreply.github.com>
2019-07-23 10:01:58 -0700

    mobile responsive

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
+    <h5>created by Ronald Reyes</h5>
     <div class="container text-capitalize">
       <h1 class="text-center display-1 m-5">{{activeType}} Pokemon Type</h1>
-      <span v-for="type in pokemon_types" class="btn btn-primary m-1" v-on:click="type_click(type.name)">{{type.name}}</span>
+      <span
+        v-for="type in pokemon_types"
+        class="btn btn-primary m-1"
+        v-on:click="type_click(type.name)"
+      >{{type.name}}</span>
       <div class="row">
         <Card :url="item.pokemon.url" v-for="item in pokemon_current" :key="item.pokemon.name"></Card>
       </div>
@@ -11,62 +16,52 @@
 </template>
 
 <script>
-
-import Card from './components/Card.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Card
   },
   data: function() {
     return {
-      pokemon_current:"",
-      pokemon_types:"",
-      activeType:"Select A"
-    }
+      pokemon_current: "",
+      pokemon_types: "",
+      activeType: "Select A"
+    };
   },
-  methods:{
+  methods: {
     type_click: function(name) {
       this.activeType = name;
-      this.retrievePokemonOfSpecifiedType(this.activeType); 
+      this.retrievePokemonOfSpecifiedType(this.activeType);
     },
     retrievePokemonOfSpecifiedType: function(type) {
-
-      const axios = require('axios');
+      const axios = require("axios");
       const vm = this;
 
-    axios({
-        method: 'get',
-        url: 'https://pokeapi.co/api/v2/type/' + type
-    })
-    .then(function (response) {
-        vm.pokemon_current = response.data.pokemon
-    });
+      axios({
+        method: "get",
+        url: "https://pokeapi.co/api/v2/type/" + type
+      }).then(function(response) {
+        vm.pokemon_current = response.data.pokemon;
+      });
     }
   },
   mounted: function() {
-    console.log("mounted function ran")
-
-    const axios = require('axios');
+    const axios = require("axios");
     const vm = this;
 
     this.retrievePokemonOfSpecifiedType(this.activeType);
 
     axios({
-        method: 'get',
-        url: 'https://pokeapi.co/api/v2/type'
-    })
-    .then(function (response) {
-        vm.pokemon_types = response.data.results
+      method: "get",
+      url: "https://pokeapi.co/api/v2/type"
+    }).then(function(response) {
+      vm.pokemon_types = response.data.results;
     });
-
-
   }
-}
-
+};
 </script>
 
 <style>
-
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,53 +1,49 @@
 <template>
-    <div class="col-3" v-if="pokemon.sprites && pokemon.sprites.front_shiny">
-        <div class="card" style="width: 18rem;">
-            <img v-bind:src="pokemon.sprites.front_shiny" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title text-capitalize">{{pokemon.name}}</h5>
-                <div>Height: {{pokemon.height*3.93701.toFixed(2)}} Inches </div>
-                <div>Weight: {{pokemon.weight*0.220462.toFixed(2)}} Pounds</div>
-            </div>
-        </div>
+  <div class="col-md-4" v-if="pokemon.sprites && pokemon.sprites.front_shiny">
+    <div class="card" style="width: 18rem;">
+      <img v-bind:src="pokemon.sprites.front_shiny" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title text-capitalize">{{pokemon.name}}</h5>
+        <p>Height: {{pokemon.height*4.0.toFixed(2)}} Inches</p>
+        <div>Weight: {{pokemon.weight*1.0.toFixed(2)}} Pounds</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Card',
-  data: function(){
-      return{
-          pokemon:""
-      }
+  name: "Card",
+  data: function() {
+    return {
+      pokemon: ""
+    };
   },
   props: {
     url: String
   },
-  mounted: function(){
-    console.log("mounted function ran")
-
-    const axios = require('axios');
+  mounted: function() {
+    const axios = require("axios");
     const vm = this;
 
     axios({
-        method: 'get',
-        url: vm.url,
-        responseType: 'stream'
-    })
-    .then(function (response){
-        vm.pokemon = response.data
+      method: "get",
+      url: vm.url,
+      responseType: "stream"
+    }).then(function(response) {
+      vm.pokemon = response.data;
     });
-
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.card{
-    margin-top: 20px;
-    text-align: center;
-    margin-bottom: 20px;
-    border: 3px solid #007bff;
-    border-radius:15px;
+.card {
+  margin-top: 20px;
+  text-align: center;
+  margin-bottom: 20px;
+  border: 3px solid #007bff;
+  border-radius: 15px;
 }
 </style>
