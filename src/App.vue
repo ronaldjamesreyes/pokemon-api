12069e70cd406e42da7a7d9319f9641f8b9da5df
Zankurou <48842512+ronaldreyes224@users.noreply.github.com>
2019-08-27 20:27:08 -0700

    reverting

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
     <div id="app">
         <h5>created by Ronald Reyes</h5>
         <div class="container text-capitalize">
@@ -8,56 +9,75 @@
                 <Card :url="item.pokemon.url" v-for="item in pokemon_current" :key="item.pokemon.name"></Card>
             </div>
         </div>
+=======
+  <div id="app">
+    <div class="container text-capitalize">
+      <h1 class="text-center display-1 m-5">{{activeType}} Pokemon Type</h1>
+      <span v-for="type in pokemon_types" class="btn btn-primary m-1" v-on:click="type_click(type.name)">{{type.name}}</span>
+      <div class="row">
+        <Card :url="item.pokemon.url" v-for="item in pokemon_current" :key="item.pokemon.name"></Card>
+      </div>
+>>>>>>> parent of 908d7a9... mobile responsive
     </div>
 </template>
 
 <script>
-import Card from "./components/Card.vue";
+
+import Card from './components/Card.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Card
   },
   data: function() {
     return {
-      pokemon_current: "",
-      pokemon_types: "",
-      activeType: "Select A"
-    };
+      pokemon_current:"",
+      pokemon_types:"",
+      activeType:"Select A"
+    }
   },
-  methods: {
+  methods:{
     type_click: function(name) {
       this.activeType = name;
-      this.retrievePokemonOfSpecifiedType(this.activeType);
+      this.retrievePokemonOfSpecifiedType(this.activeType); 
     },
     retrievePokemonOfSpecifiedType: function(type) {
-      const axios = require("axios");
+
+      const axios = require('axios');
       const vm = this;
 
-      axios({
-        method: "get",
-        url: "https://pokeapi.co/api/v2/type/" + type
-      }).then(function(response) {
-        vm.pokemon_current = response.data.pokemon;
-      });
+    axios({
+        method: 'get',
+        url: 'https://pokeapi.co/api/v2/type/' + type
+    })
+    .then(function (response) {
+        vm.pokemon_current = response.data.pokemon
+    });
     }
   },
   mounted: function() {
-    const axios = require("axios");
+    console.log("mounted function ran")
+
+    const axios = require('axios');
     const vm = this;
 
     this.retrievePokemonOfSpecifiedType(this.activeType);
 
     axios({
-      method: "get",
-      url: "https://pokeapi.co/api/v2/type"
-    }).then(function(response) {
-      vm.pokemon_types = response.data.results;
+        method: 'get',
+        url: 'https://pokeapi.co/api/v2/type'
+    })
+    .then(function (response) {
+        vm.pokemon_types = response.data.results
     });
+
+
   }
-};
+}
+
 </script>
 
 <style>
+
 </style>
